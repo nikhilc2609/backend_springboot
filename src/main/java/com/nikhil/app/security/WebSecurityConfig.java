261ec7d5c3d553bc839de7d8051a163ff1a0e6bf
Nikhil C <nikhil.c@tavant.com>
2021-06-05 18:49:44 +0530

    1

--- src/main/java/com/nikhil/app/security/WebSecurityConfig.java
+++ src/main/java/com/nikhil/app/security/WebSecurityConfig.java
@@ -54,8 +54,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().csrf().disable()
 			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-			.authorizeRequests().antMatchers("/api/**").permitAll()
-			.antMatchers("/api/**").permitAll()
+			.authorizeRequests().antMatchers("https://springboot-probability.herokuapp.com/api/**").permitAll()
+			.antMatchers("https://springboot-probability.herokuapp.com/api/**").permitAll()
 			.anyRequest().authenticated();
 
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
